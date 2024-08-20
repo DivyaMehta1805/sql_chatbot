ba70bb6f4f064d3cd90e76b2a5277e9eacd3e6b2
Divya Mehta <93784062+DivyaMehta1805@users.noreply.github.com>
2024-08-20 12:35:38 +0530

    Add files via upload

--- sql_rag-main/python_apisetup.py
+++ sql_rag-main/python_apisetup.py
@@ -1,12 +1,24 @@
 from flask import Flask, request, jsonify
+import json
 from flask_cors import CORS
 from database_api import process
 import pandas as pd
+
+
 app = Flask(__name__)
 CORS(app)
 last_query_result = None
 previous_query = ""
 result=""
+
+def custom_json_encoder(obj):
+    if isinstance(obj, str):
+        return obj.replace('\n', '\\n')
+    return obj
+
+app = Flask(__name__)
+CORS(app)
+
 @app.route('/api/query', methods=['POST'])
 def handle_query():
     global result
@@ -22,14 +34,18 @@ def handle_query():
         return jsonify({'status': 'Query processed'})
     else:
         return jsonify({'status': 'No query received'})
+
 @app.route('/api/result', methods=['GET'])
 def get_result():
-    
     if isinstance(result, pd.DataFrame):
         result_list = result.to_dict(orient='records')
         return jsonify({'response': result_list})
     elif result is not None:
-        return jsonify({'response': result})
+        # Use json.dumps with the custom encoder
+        return app.response_class(
+            json.dumps({'response': result}, default=custom_json_encoder),
+            mimetype='application/json'
+        )
     else:
         return jsonify({'response': 'No result available'})
 
